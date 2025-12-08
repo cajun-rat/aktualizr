7128f83e1aa171741e17dd915d186ca513120982
Phil Wise <phil@phil-wise.com>
2025-12-08 20:09:39 +0000

    WIP

--- src/libaktualizr/primary/aktualizr_offline_update_test.cc
+++ src/libaktualizr/primary/aktualizr_offline_update_test.cc
@@ -103,6 +103,7 @@ TEST_F(AktualizrOfflineUpdate, AktualizrInitWithLockBox) {  // NOLINT
       << "Image root metadata should be imported";
   EXPECT_FALSE(image_root.empty()) << "Image root metadata should not be empty";
 
+  // TODO: This doesn't work. Need to do it via the state machine
   auto update_result = dut.CheckUpdatesOffline(lockbox_dir_).get();
 }
 
